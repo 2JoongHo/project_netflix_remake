52ff750b91c7b050ae1320bc9ae901b2bf7e4dfa
Joongho <honor9320@gmail.com>
2026-02-26 23:58:11 +0900

    넷플릭스 기본 틀 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import logo from "./assets/Netflix_logo.svg";
+import search_btn from "./assets/search_btn.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {/* 네비게이션 바 */}
+      <nav className="nav-bar">
+        <img className="nav-logo" src={logo} alt="NETFLIX" />
+        <div className="nav-left">
+          <button>시리즈</button>
+          <button>영화</button>
+        </div>
+        {/* 검색 버튼 */}
+        <img className="search_btn" src={search_btn} alt="search_btn" />
+      </nav>
+
+      {/* 메인 배너 */}
+      <header className="main-banner">
+        <h2>영화 제목</h2>
+        <div className="banner-buttons">
+          <button>▶ 재생</button>
+          <button>ⓘ 상세 정보</button>
+        </div>
+      </header>
+
+      {/* 영화 리스트 슬라이드 */}
+      <section className="row">
+        <h3>지금 뜨는 콘텐츠</h3>
+        <div className="posters">
+          <div className="poster">이미지 1</div>
+          <div className="poster">이미지 2</div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
